32683fcd02ca1d3d15fcc79a735fb520fe528487
helenakolodko <helena.kolodko@gmail.com>
2015-07-14 15:52:17 +0300

    Made separate test methods with exceptions and without

--- Task1.Tests/NewtonRootTest.cs
+++ Task1.Tests/NewtonRootTest.cs
@@ -23,21 +23,19 @@ namespace Task1.Tests
             Double value = Convert.ToDouble(TestContext.DataRow["Value"]);
             int degree = Convert.ToInt32(TestContext.DataRow["Degree"]); 
             Double precision = Convert.ToDouble(TestContext.DataRow["Precision"]);
-            bool exception = Convert.ToBoolean(TestContext.DataRow["Exception"]);
-            bool catched = false;
-            try
-            {
-                Double actual = Calculator.NewtonRoot(value, degree, precision);
-                Assert.AreEqual(Math.Pow(value, 1d / degree), actual, precision);
+            Double actual = Calculator.NewtonRoot(value, degree, precision);
+            Assert.AreEqual(Math.Pow(value, 1d / degree), actual, precision); 
+        }
 
-            }
-            catch(ArgumentException e){
-                catched = true;
-            }
-            if (catched != exception)
-            {
-                Assert.Fail();
-            }
+        [DataSource(connectionString, "NewtonRootTestDataExceptions#csv")]
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewtonRoot_FromDataSourceTest_ThrowsException()
+        {
+            Double value = Convert.ToDouble(TestContext.DataRow["Value"]);
+            int degree = Convert.ToInt32(TestContext.DataRow["Degree"]);
+            Double precision = Convert.ToDouble(TestContext.DataRow["Precision"]);
+            Calculator.NewtonRoot(value, degree, precision);
         }
     }
 }
